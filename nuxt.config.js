e984e73f2b972fb9c06c2c1e83cc6118495126e2
ehoraizon <erich.info.work@gmail.com>
2022-05-24 22:49:51 -0400

    Adds small fixes 😅

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/vue-commits/'
+    base: '/vue-commits/',
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
