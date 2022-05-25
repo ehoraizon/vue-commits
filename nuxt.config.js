7aa2c15a3b2988a69039a0e2acb2a077c64f8ec5
ehoraizon <erich.info.work@gmail.com>
2022-05-24 22:43:53 -0400

    Adds gh nuxt static site configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/vue-commits/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'vue-commits',
